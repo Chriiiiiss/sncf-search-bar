c5691908aa7c4030cc5bdff97c1bb09c8a8b3856
Chris <chris.fraanc@gmail.com>
2023-04-23 00:47:47 +0200

    [ADD] Adding some UI stuffs, creating a whole component for the search bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,12 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import SearchBar from "./components/SearchBar";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="flex justify-center gap-10 items-center flex-col h-screen">
+        <h1 className="text-4xl font-bold text-red">Search Bar</h1>
+        <SearchBar />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
