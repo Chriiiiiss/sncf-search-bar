3f001fe894ef3f672b41603fb311867425c0e0a0
Chris <chris.fraanc@gmail.com>
2023-04-23 00:50:44 +0200

    [MOD] modifying background to a darker themes

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <>
       <div className="flex justify-center gap-10 items-center flex-col h-screen">
-        <h1 className="text-4xl font-bold text-red">Search Bar</h1>
+        <h1 className="text-4xl font-bold text-white">Search Bar</h1>
         <SearchBar />
       </div>
     </>
