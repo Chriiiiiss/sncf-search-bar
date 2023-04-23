ea8c02b3f6968a67c0d3acd37cf59b590d569876
Chris <chris.fraanc@gmail.com>
2023-04-23 19:07:01 +0200

    [MOD] Modified some Card Css Issues

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -10,25 +10,25 @@ interface ICard {
 
 const Card: React.FC<ICard> = ({ title, imgUrl, ctaLabel, description }) => {
   return (
-    <div className="flex flex-col bg-white rounded-lg cardContainer">
-      <img className="rounded-t-lg" src={imgUrl} />
-      <div className="flex flex-col gap-10">
-        <div className="p-4">
+    <div className="flex flex-col bg-white rounded-lg cardContainer justify-between">
+      <div className="flex flex-col">
+        <img className="rounded-t-lg" src={imgUrl} />
+        <div className="px-4 pt-4">
           <h3 className="font-bold text-md avenirBoldFont mb-2">{title}</h3>
           <p className="avenirFont text-slate-600">{description}</p>
         </div>
-        <div className="linkCta">
-          <span className="separator" />
-          <a
-            className="flex items-center gap-2 justify-center pb-4 px-4"
-            href="#"
-          >
-            <span className="text-xs">{ctaLabel}</span>
-            <span>
-              <ArrowSvg className="transform rotate-90" />
-            </span>
-          </a>
-        </div>
+      </div>
+      <div className="linkCta mt-4">
+        <span className="separator" />
+        <a
+          className="flex items-center gap-2 justify-center pb-4 px-4"
+          href="#"
+        >
+          <span className="text-xs">{ctaLabel}</span>
+          <span>
+            <ArrowSvg className="transform rotate-90" />
+          </span>
+        </a>
       </div>
     </div>
   );
